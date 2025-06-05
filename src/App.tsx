8e985ec852ca8a7adceed5ebbbd8b52c61bcced4
eulucaslim <lucaslimacnt14@gmail.com>
2025-06-04 22:16:22 -0400

    Create the component NewTransaction Modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Modal from 'react-modal';
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -21,12 +22,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-      <Modal 
-        isOpen={isNewTransactionModalOpen} 
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-       <h2> Cadastrar transação </h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
